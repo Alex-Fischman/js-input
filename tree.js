6e485ddd6989513d7a79faad20702162e038b48d
Alex <alex@fischman.org>
2019-08-23 14:06:54 -0700

    Changes from link in description
    https://codereview.stackexchange.com/questions/226709/javascript-tree-class

--- tree.js
+++ tree.js
@@ -1,61 +1,63 @@
-function Node(value = 0, children = []) {
-	this.value = value;
-	this.children = children;
-	this.parent = undefined;
+class Node {
+	constructor(value = 0, children = []) {
+		this.value = value;
+		this.children = children;
+	}
+
+	clone() {
+		let that = Object.assign(new Node(), this);
+		that.children = this.children.map(n => n.clone());
+		return that;
+	}
+
+	add(...children) {
+		this.children = this.children.concat(children);
+		return this;
+	}
+
+	traverse(callback, traversal = Node.Traversal.BreadthFirst) {
+		traversal.call(this, callback);
+		return this;
+	}
+
+	reduce(callback, initial, traversal) {
+		let a = initial;
+		this.traverse(n => a = callback(a, n), traversal);
+		return a;
+	}
+
+	every(callback) {
+		return this.reduce((a, n) => a && callback(n), true);
+	}
+
+	some(callback) {
+		return this.reduce((a, n) => a || callback(n), false);
+	}
+
+	find(callback, traversal) {
+		return this.reduce((a, n) => a || (callback(n)? n: false), false, traversal);
+	}
+
+	includes(value) {
+		return this.some(n => n.value === value);
+	}
 }
 
-Node.from = function(node) {
-	let that = Object.assign(new Node(), node);
-	that.parent = undefined;
-	that.children = node.children.map(n => (Node.from(n), n.parent = that));
-	return that;
-};
-
-Node.prototype.add = function(...children) {
-	for (child of children) {
-		child.parent = this;
-		this.children.push(child);
-	}
-};
-
 Node.Traversal = {
-	BreadthFirst: 1,
-	DepthFirst: 2
-};
-
-Node.prototype.traverse = function(callback, mode = Node.Traversal.BreadthFirst) {
-	if (mode == Node.Traversal.BreadthFirst) {
+	BreadthFirst: function(callback) {
 		let nodes = [this];
 		while (nodes.length > 0) {
 			const current = nodes.shift();
 			callback(current);
 			nodes = nodes.concat(current.children);
-		}
-	} else if (mode == Node.Traversal.DepthFirst) {
+		}      
+	},
+	DepthFirstPreOrder: function(callback) {
 		callback(this);
-		this.children.forEach(n => n.traverse(callback, false));
+		this.children.forEach(n => n.traverse(callback, Node.Traversal.DepthFirst));
+	},
+	DepthFirstPostOrder: function(callback) {
+		this.children.forEach(n => n.traverse(callback, Node.Traversal.DepthFirstPostOrder));
+		callback(this);
 	}
-	return this;
-};
-
-Node.prototype.reduce = function(callback, initial, mode) {
-	let acc = initial;
-	this.traverse(n => acc = callback(acc, n), mode);
-	return acc;
-};
-
-Node.prototype.every = function(callback) {
-	return this.reduce((a, n) => a && callback(n), true);
-};
-
-Node.prototype.some = function(callback) {
-	return this.reduce((a, n) => a || callback(n), false);
-};
-
-Node.prototype.find = function(callback, mode) {
-	return this.reduce((a, n) => a || (callback(n)? n: false), false, mode);
-};
-
-Node.prototype.includes = function(value) {
-	return this.some(n => n.value === value);
 };

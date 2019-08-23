92e53f4518ea4b1d546256128b4037043747b01a
Alex <alex@fischman.org>
2019-08-23 02:50:40 -0700

    Whole bunch of stuff
    Changed clone to Node.from constructor, added traversal enum, added reduce, implemented find and includes using reduce.

--- tree.js
+++ tree.js
@@ -4,40 +4,61 @@ function Node(value = 0, children = []) {
 	this.parent = undefined;
 }
 
+Node.from = function(node) {
+	let tree = Object.assign(new Node(), node);
+	tree.children = node.children.map(n => Node.from(n));
+	return tree;
+};
+
+Node.root = function(node) {
+	return node.parent === undefined;
+}
+
 Node.prototype.add = function(...children) {
 	for (child of children) {
 		child.parent = this;
 		this.children.push(child);
 	}
-	return this.children.length;
 };
 
-Node.prototype.traverse = function(callback, breadthFirst = true) {
-	if (breadthFirst) {
+Node.Traversal = {
+	BreadthFirst: 1,
+	DepthFirst: 2
+};
+
+Node.prototype.traverse = function(callback, mode = Node.Traversal.BreadthFirst) {
+	if (mode == Node.Traversal.BreadthFirst) {
 		let nodes = [this];
 		while (nodes.length > 0) {
 			const current = nodes.shift();
 			callback(current);
 			nodes = nodes.concat(current.children);
 		}
-	} else {
+	} else if (mode == Node.Traversal.DepthFirst) {
 		callback(this);
-		this.children.forEach(c => c.traverse(callback, false));
+		this.children.forEach(n => n.traverse(callback, false));
 	}
 };
 
-Node.prototype.clone = function() {
-	let that = Object.assign(new Node(), this);
-	that.children = that.children.map(c => c.clone());
-	return that;
+Node.prototype.reduce = function(callback, initial, mode) {
+	let acc = initial === undefined? this: initial;
+
+	this.traverse(function(node) {
+		if (!Node.root(node) || initial !== undefined) {
+			acc = callback(acc, node);
+		}
+	}, mode);
+
+	return acc;
 };
 
-Node.prototype.find = function(callback, breadthFirst = true) {
-	let result = undefined;
-	this.traverse(n => { if (callback(n) && result === undefined) { result = n; } }, breadthFirst);
-	return result;
+Node.prototype.includes = function(value, mode) {
+	return this.reduce((a, n) => a || n.value === value, false, mode);
 };
 
-Node.prototype.includes = function(value) {
-	return Boolean(this.find(n => n.value === value));
+Node.prototype.find = function(callback, mode) {
+	return this.reduce((a, n) => a || (callback(n)? n: false), false, mode);
 };
+
+Node.prototype.every;
+Node.prototype.some;

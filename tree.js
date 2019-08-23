301803ee2e4d223ad2a737637089c099794c42df
Alex <alex@fischman.org>
2019-08-22 18:26:08 -0700

    Add iteration functions, declare other functions

--- tree.js
+++ tree.js
@@ -2,3 +2,31 @@ function Node(value, children) {
 	this.value = value || 0;
 	this.children = children || [];
 }
+
+Node.prototype.depth = function(callback) {
+	callback(this.value);
+	for (child of this.children) {
+		child.depth(callback);
+	}
+};
+
+Node.prototype.breadth = function(callback) {
+	let nodes = [this];
+	while (nodes.length > 0) {
+		const current = nodes.shift();
+		nodes = nodes.concat(current.children);
+		callback(current.value);
+	}
+}
+
+Node.prototype.map;
+
+Node.prototype.filter;
+
+Node.prototype.reduce;
+
+Node.prototype.every;
+Node.prototype.fill;
+Node.prototype.includes;
+Node.prototype.some;
+Node.prototype.find; // dfs and bfs?

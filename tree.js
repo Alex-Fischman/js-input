566811fc363dd13770bc0bf4a0a5a2137d7e0be5
Alex <alex@fischman.org>
2019-08-23 16:15:29 -0700

    Added filter, moved clone

--- tree.js
+++ tree.js
@@ -4,12 +4,6 @@ class Node {
 		this.children = children;
 	}
 
-	clone() {
-		let that = Object.assign(new Node(), this);
-		that.children = this.children.map(n => n.clone());
-		return that;
-	}
-
 	add(...children) {
 		this.children = this.children.concat(children);
 		return this;
@@ -32,12 +26,32 @@ class Node {
 		return a;
 	}
 
+	filter(callback) {
+		let that = this.clone();
+		that.traverse((n, t) => {
+			if (!callback(n)) {
+				const parent = that.find(m => m.children.includes(n));
+				if (!parent) return; // Can't return root node, but would rather make filter also return undefined
+				const index = parent.children.findIndex(o => o === n);
+				parent.children.splice(index, 1);
+			}
+		}, Node.Traversal.BreadthFirst);
+		return that;
+	}
+
 	every(callback) {
 		return this.reduce((a, n) => a && callback(n), true);
 	}
 
 	some(callback) {
 		return this.reduce((a, n) => a || callback(n), false);
+	}
+
+	// Can this be implemented using traverse?
+	clone() {
+		let that = Object.assign(new Node(), this);
+		that.children = this.children.map(n => n.clone());
+		return that;
 	}
 
 	find(callback, traversal) {
@@ -60,7 +74,7 @@ Node.Traversal = {
 	},
 	DepthFirstPreOrder: function(callback) {
 		callback(this);
-		this.children.forEach(n => n.traverse(callback, Node.Traversal.DepthFirst));
+		this.children.forEach(n => n.traverse(callback, Node.Traversal.DepthFirstPreOrder));
 	},
 	DepthFirstPostOrder: function(callback) {
 		this.children.forEach(n => n.traverse(callback, Node.Traversal.DepthFirstPostOrder));

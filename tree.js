66a55c622630bb7da9d1073cf7572d78e61821f1
Alex <alex@fischman.org>
2019-08-23 00:55:19 -0700

    Implemented map

--- tree.js
+++ tree.js
@@ -1,47 +1,46 @@
 function Node(value = 0, children = []) {
 	this.value = value;
 	this.children = children;
-	this.depth = 0;
-	this.breadth = 0;
+	this.parent = undefined;
 }
 
 Node.prototype.push = function(...children) {
 	for (child of children) {
-		child.depth = this.depth + 1;
-		child.breadth = this.children.length;
+		child.parent = this;
 		this.children.push(child);
 	}
 	return this.children.length;
 };
 
-Node.prototype.pop = function() { return this.children.pop(); };
+Node.prototype.pop = function() {
+	return this.children.pop();
+};
 
 Node.prototype.traverse = function(callback, breadthFirst = true) {
 	if (breadthFirst) {
-		// Breadth First Traversal
 		let nodes = [this];
 		while (nodes.length > 0) {
 			const current = nodes.shift();
-			callback(current.value, current.depth, current.breadth);
+			callback(current);
 			nodes = nodes.concat(current.children);
 		}
 	} else {
-		// Depth First Traversal
-		callback(this.value, this.depth, this.breadth);
+		callback(this);
 		this.children.forEach(c => c.traverse(callback, false));
 	}
 };
 
 Node.prototype.map = function(callback) {
-	this.traverse(function() {});
+	let that = Object.assign(new Node(), this);
+	that.children = that.children.map(c => callback(c.map(callback)));
+	return that;
 };
 
 Node.prototype.filter;
-
 Node.prototype.reduce;
 
 Node.prototype.every;
 Node.prototype.fill;
 Node.prototype.includes;
 Node.prototype.some;
-Node.prototype.find; // dfs and bfs?
+Node.prototype.find;

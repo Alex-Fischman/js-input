db986fc757a441b0a870611255c5ebe7d696a55e
Alex <alex@fischman.org>
2020-12-10 23:17:11 -0800

    Added scroll wheel support

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
-let input = {};
+let input = {wheel: 0};
 window.onkeydown     = e => input[event.key] = true;
 window.onkeyup       = e => input[event.key] = false;
 window.onpointerdown = _ => input.pressed    = true;
 window.onpointerup   = _ => input.pressed    = false;
-window.onpointermove = e => input.position   = [e.x, e.y];
+window.onpointermove = e => input.position   = {x: e.x, y: e.y};
+window.onwheel       = e => input.wheel     += e.deltaY;

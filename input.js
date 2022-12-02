7502a6bdc39fef0ed9131bc586959e53098e573b
Alex <alex@fischman.org>
2022-12-02 12:11:06 -0800

    Update input.js

--- input.js
+++ input.js
@@ -1,7 +1,41 @@
-let input = {wheel: 0};
-document.addEventListener("keydown",     e => input[event.key] = true);
-document.addEventListener("keyup",       e => input[event.key] = false);
-document.addEventListener("pointerdown", _ => input.pressed    = true);
-document.addEventListener("pointerup",   _ => input.pressed    = false);
-document.addEventListener("pointermove", e => input.position   = {x: e.x, y: e.y});
-document.addEventListener("wheel",       e => input.wheel     += e.deltaY);
+const keys = {};
+const keysDownThisFrame = {};
+const keysUpThisFrame = {};
+document.addEventListener("keydown", event => {
+	keys[event.key] = true;
+	keysDownThisFrame[event.key] = true;
+});
+document.addEventListener("keyup", event => {
+	keys[event.key] = false;
+	keysUpThisFrame[event.key] = true;
+});
+
+const mouseButtons = {};
+const mouseButtonsDownThisFrame = {};
+const mouseButtonsUpThisFrame = {};
+document.addEventListener("mousedown", () => {
+	mouse[event.button] = true;
+	mouseButtonsDownThisFrame[event.button] = true;
+});
+document.addEventListener("mouseup", () => {
+	mouseButtons[event.button] = false;
+	mouseButtonsUpThisFrame[event.button] = true;
+});
+
+function key        (key) { return !!keys[key]; }
+function keyPressed (key) { return !!keysDownThisFrame[key]; }
+function keyReleased(key) { return !!keysUpThisFrame[key]; }
+
+function mouse        (button) { return !!mouseButtons[button]; }
+function mousePressed (button) { return !!mouseButtonsDownThisFrame[button]; }
+function mouseReleased(button) { return !!mouseButtonsUpThisFrame[button]; }
+
+function updateInput() {
+	for (const i in keysDownThisFrame) delete keysDownThisFrame[i];
+	for (const i in keysUpThisFrame)   delete keysUpThisFrame[i];
+	for (const i in mouseButtonsDownThisFrame) delete mouseButtonsDownThisFrame[i];
+	for (const i in mouseButtonsUpThisFrame)   delete mouseButtonsUpThisFrame[i];
+}
+
+document.addEventListener("pointermove", e => mouse.position   = {x: e.x, y: e.y});
+document.addEventListener("wheel",       e => mouse.wheel     += e.deltaY);
